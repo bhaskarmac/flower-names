6c1c57684884c8229806535f2fbe2dfa5074154a
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-11-03 17:11:44 +0530

    Adding first test with mocha

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var flowers = require('./index');
+
+describe('flower-names', function () {
+	it('should work', function(){
+		expect(true).to.be.true;
+	});
+});
