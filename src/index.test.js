54f724c43564f606f0af00ee057bf5c96a985b8c
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-11-08 21:23:46 +0530

    Adding test for array of strings

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,14 @@
 var expect = require('chai').expect;
 var flowers = require('./index');
 
-describe('flower-names', function () {
-	it('should work', function(){
-		expect(true).to.be.true;
+describe('all', function () {
+	it('should be array of strings', function(){
+		expect(flowers.all).to.satisfy(isArrayOfStrings);
+
+		function isArrayOfStrings(array){
+			return array.every(function(item){
+				return typeof item === 'string';
+			});
+		}
 	});
 });
