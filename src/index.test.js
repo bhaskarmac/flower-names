a307c9f82293e6f8f2bd662fb0ce14734d6303d4
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-11-09 19:15:33 +0530

    Adding some more tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,14 +1,31 @@
 var expect = require('chai').expect;
 var flowers = require('./index');
 
-describe('all', function () {
-	it('should be array of strings', function(){
-		expect(flowers.all).to.satisfy(isArrayOfStrings);
+describe('flower-names', function () {
+	describe('all', function () {
 
-		function isArrayOfStrings(array){
-			return array.every(function(item){
-				return typeof item === 'string';
-			});
-		}
+		it('should be array of strings', function(){
+			expect(flowers.all).to.satisfy(isArrayOfStrings);
+
+			function isArrayOfStrings(array){
+				return array.every(function(item){
+					return typeof item === 'string';
+				});
+			}
+		});
+
+
+		it('should contain Jasmine', function () {
+			expect(flowers.all).to.include('Jasmine');
+		});
+
 	});
+
+	describe('random', function(){
+		it('should return random item from flower.all', function () {
+			var randomItem = flowers.random();
+			expect(flowers.all).to.include(randomItem);
+		});
+	});
+	
 });
